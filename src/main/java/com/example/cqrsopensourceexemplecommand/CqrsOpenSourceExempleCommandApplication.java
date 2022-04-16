396ea05c7098704bb4809747bd2b5bd5cb1706a3
victor <victor.eduardo.sntos@gmail.com>
2022-04-16 16:55:42 -0300

    Initial commit

---
+++ src/main/java/com/example/cqrsopensourceexemplecommand/CqrsOpenSourceExempleCommandApplication.java
@@ -0,0 +1,13 @@
+package com.example.cqrsopensourceexemplecommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsOpenSourceExempleCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsOpenSourceExempleCommandApplication.class, args);
+    }
+
+}
